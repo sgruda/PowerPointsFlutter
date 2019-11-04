ede4cd9c3ade388da262fc7c79368b855a634bac
Szymon Gruda <szymon_gruda@o2.pl>
2019-11-04 11:09:51 +0100

    Availability of looking at current location (why is it google home?).

--- power_points/lib/main.dart
+++ power_points/lib/main.dart
@@ -1,44 +1,108 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
+
+CurrentLocation cur = new CurrentLocation();
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-           body: FireMap()
+           body: FireMap(cur),
+            appBar: AppBar(
+              title: Text('Latitude: ' + cur.getString("latitude") + '\nLongitude: ' + cur.getString("longitude")),
+    ),
+
+    )
          // body: Center(
         //    child: Text('Hello World'),
        //   ),
-        )
+       // )
     );
   }
 }
 
 class FireMap extends StatefulWidget {
   @override
-  State createState() => FireMapState();
+  CurrentLocation location;
+  FireMap(CurrentLocation currentLocation) {
+    location = currentLocation;
+  }
+  State createState() => FireMapState(location);
 }
 
+class CurrentLocation {
+  double latitude;
+  double longitude;
+  _getLocation() async {
+    var location = new Location();
+    try {
+       var currentLocation = await location.getLocation();
+      latitude = currentLocation["latitude"];
+      longitude = currentLocation["longitude"];
+      // print("locationLatitude: ${currentLocation["latitude"]}");
+      // print("locationLongitude: ${currentLocation["longitude"]}");
+     // setState(
+            //  () {}); //rebuild the widget after getting the current location of the user
+    } on Exception {
 
+    }
+  }
+  String getString(String what) {
+    _getLocation();
+    if(what == "latitude") return latitude.toString();
+    else if ( what == "longitude")return longitude.toString();
+    return super.toString();
+  }
+}
 class FireMapState extends State<FireMap> {
   GoogleMapController mapController;
-
+  var currentLocation;
+  CurrentLocation myLocation;
+  FireMapState(CurrentLocation currentLocation) {
+    myLocation = currentLocation;
+  }
   build(context) {
     return Stack(
         children: [
           GoogleMap(
-            initialCameraPosition: CameraPosition(target: LatLng(51.5905428,19.1432884), zoom: 10),
+            initialCameraPosition: CameraPosition(
+              target: LatLng(51.589496, 19.158193),
+                //target: LatLng(51.747300, 19.453670),
+                //target: LatLng(currentLocation['latitude'], currentLocation['longitude']),
+                zoom: 15
+            ),
+            onMapCreated: _onMapCreated,
+            myLocationEnabled: true,
+            mapType: MapType.hybrid,
+            compassEnabled: true,
+            trackCameraPosition: true,
           ),
+
         ]
     );
   }
-
   void _onMapCreated(GoogleMapController controller) {
+//    _getLocation();
+    _animateToUser();
     setState(() {
       mapController = controller;
     });
+  }
+  _animateToUser() async {
+    var location = new Location();
+    var pos = await location.getLocation();
+    mapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(pos['latitude'], pos['longitude']),
+          zoom: 17.0,
+        )
+    )
+    );
   }
 }
