3b5385435d0022d127b894953decda9168365389
Szymon Gruda <szymon_gruda@o2.pl>
2019-11-08 19:55:20 +0100

    Add simple checking location of markers and user.  And info pop-up.

--- power_points/lib/main.dart
+++ power_points/lib/main.dart
@@ -7,6 +7,7 @@ import 'dart:async';
 void main() => runApp(MyApp());
 
 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -30,8 +31,8 @@ class FireMapState extends State<FireMap> {
         children: [
           GoogleMap(
             initialCameraPosition: CameraPosition(
-//                target: LatLng(51.589496, 19.158193),                           // Home
-                target: LatLng(51.747300, 19.453670),                         // Polibuda
+                target: LatLng(51.589496, 19.158193),                           // Home
+//                target: LatLng(51.747300, 19.453670),                         // Polibuda
                 zoom: 15
             ),
             onMapCreated: _onMapCreated,
@@ -46,6 +47,7 @@ class FireMapState extends State<FireMap> {
   void _onMapCreated(GoogleMapController controller) {
     _addMarkers(controller);
     _animateToUser();
+    _checkPoints();
     setState(() {
       mapController = controller;
     });
@@ -63,7 +65,8 @@ class FireMapState extends State<FireMap> {
   }
   void _addMarkers(GoogleMapController controller) {
     var marker = MarkerOptions(
-        position: LatLng(51.7474, 19.4537),
+//        position: LatLng(51.7474, 19.4537),                                       //Polibuda
+        position: LatLng(51.58949, 19.15819),                                     //Home
         icon: BitmapDescriptor.defaultMarker,
         infoWindowText: InfoWindowText("Jej, udalo sie!","")
     );
@@ -72,13 +75,37 @@ class FireMapState extends State<FireMap> {
   void _checkPoints() async {
     var location = new Location();
     var pos = await location.getLocation();
-
-    if( abs(pos.latitude - 51.747 ) < 0.001 && abs(pos.longitude - 19.4537 ) < 0.001) {
-
+    if( abs(pos.latitude - 51.747 ) < 1 && abs(pos.longitude - 19.4537 ) < 1) {
+      _neverSatisfied(context, "Brawo", "Udało Ci się!");
     }
-    );
   }
   double abs(double x) {
     return x < 0 ? -x : x;
+  }
+  Future<void> _neverSatisfied(BuildContext context, String title, String text) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(text),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
