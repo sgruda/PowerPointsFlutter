070bd979e09ce7edaa348097a8e3e080236d4a38
jklepacz98 <51494383+jklepacz98@users.noreply.github.com>
2019-11-12 02:55:28 +0100

    Dodanie GUI
    -Dodanie AppBar
    -Dodanie Drawer
    -Dodanie strony dla każdej opcji z menu

--- power_points/lib/main.dart
+++ power_points/lib/main.dart
@@ -11,9 +11,184 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          body: FireMap()
+        home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FireMap(),
+        appBar: AppBar(
+          title: Text('Mapka'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.white,
+            child: ListView(
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: <Color>[
+                      Colors.deepOrange[500],
+                      Colors.orange
+                    ])
+                  ),
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                        Material(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          elevation: 10,
+                          color: Colors.white,
+                          child: Icon(Icons.location_on, size: 100,color: Colors.red,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text('Mapka: The Game',style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                        ),
+                      ],
+                    ),
+
+                  )
+                ),
+              CustomListTile(Icons.person, 'Profile', () {Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileMenu()),);}),
+              CustomListTile(Icons.monetization_on, 'Points', () {Navigator.push(context, MaterialPageRoute(builder: (context) => PointsMenu()),);}),
+              CustomListTile(Icons.comment, 'Trivia', () {Navigator.push(context, MaterialPageRoute(builder: (context) => TriviaMenu()),);}),
+              CustomListTile(Icons.settings, 'Settings', () {Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsMenu()),);}),
+              ],
+            ),
+          ),
         )
+    );
+  }
+}
+
+class ProfileMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class PointsMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Points"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class TriviaMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trivia"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget{
+
+  IconData icon;
+  String text;
+  Function onTap;
+
+  CustomListTile(this.icon, this.text, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0,0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.black12))
+        ),
+        child: InkWell(
+            splashColor: Colors.grey,
+            onTap: onTap,
+            child: Container(
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                      children: <Widget>[
+                        Icon(icon),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(text, style: TextStyle(
+                              fontSize: 16.0
+                          ),
+                          ),
+                        ),
+                      ]
+                  ),
+                  Icon(Icons.arrow_right),
+                ],
+              ),
+            )
+        ),
+      ),
     );
   }
 }
@@ -76,9 +251,9 @@ class FireMapState extends State<FireMap> {
     if( abs(pos.latitude - 51.747 ) < 0.001 && abs(pos.longitude - 19.4537 ) < 0.001) {
 
     }
-    );
   }
   double abs(double x) {
     return x < 0 ? -x : x;
   }
+
 }
