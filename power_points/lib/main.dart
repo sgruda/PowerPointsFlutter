9f8f4d2c50f90018c2b1b5f8a7cd08fa662a01a8
jklepacz98 <51494383+jklepacz98@users.noreply.github.com>
2019-11-30 17:34:43 +0100

    Fixed button location
    -Changed floatingActionButton (FAB) location
    -Changed FAB size

--- power_points/lib/main.dart
+++ power_points/lib/main.dart
@@ -7,9 +7,9 @@ import 'dart:async';
 
 void main() => runApp(MyApp());
 
-
-int userPoints = 40;
-String userName = "Jakobo Klepaczczo";
+bool instruction = true;
+int userPoints = 0;
+String userName = "Jakub Klepacz";
 
 class MyApp extends StatelessWidget {
   @override
@@ -23,52 +23,133 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FireMap(),
-        appBar: AppBar(
-          title: Text('Mapka'),
-          backgroundColor: Colors.deepOrange,
-        ),
-        drawer: Drawer(
-
-
-            child: ListView(
-              children: <Widget>[
-                DrawerHeader(
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(colors: <Color>[
-                          Colors.deepOrange[500],
-                          Colors.orange
-                        ])
-                    ),
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          CircleAvatar(
-                            backgroundColor: Colors.white,
-                            radius: 50,
-                            child: Icon(Icons.location_on, size: 100,color: Colors.blue,),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: Text('Mapka: The Game',style: TextStyle(color: Colors.white, fontSize: 25.0)),
-                          ),
-                        ],
-                      ),
-
-                    )
+      body: FireMap(),
+      appBar: AppBar(
+        title: Text('Mapka'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: <Color>[
+                      Colors.deepOrange[500],
+                      Colors.orange
+                    ])
                 ),
-                CustomListTile(Icons.person, 'Profile', () {Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileMenu()),);}),
-                CustomListTile(Icons.monetization_on, 'Points', () {Navigator.push(context, MaterialPageRoute(builder: (context) => PointsMenu()),);}),
-                CustomListTile(Icons.comment, 'Trivia', () {Navigator.push(context, MaterialPageRoute(builder: (context) => TriviaMenu()),);}),
-                CustomListTile(Icons.settings, 'Settings', () {Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsMenu()),);}),
-              ],
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50,
+                        child: Icon(Icons.location_on, size: 100,color: Colors.blue,),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text('Mapka: The Game',style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                      ),
+                    ],
+                  ),
+                )
             ),
+            CustomListTile(Icons.person, 'Profile', () {Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileMenu()),);}),
+            CustomListTile(Icons.monetization_on, 'Points', () {Navigator.push(context, MaterialPageRoute(builder: (context) => PointsMenu()),);}),
+//            CustomListTile(Icons.comment, 'Trivia', () {Navigator.push(context, MaterialPageRoute(builder: (context) => TriviaMenu()),);}),
+//            CustomListTile(Icons.settings, 'Settings', () {Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsMenu()),);}),
+          ],
+        ),
+      ),
+      floatingActionButton: Container(
+        height: 60,
+        width: 200,
+        child: FittedBox(
+          child: FloatingActionButton.extended(
+            label: Text("Sprawdź"),
+            onPressed: () {
+              _checkPoints(context);
+            },
+            icon: Icon(Icons.navigation),
+            backgroundColor: Colors.deepOrange,
           ),
+        ),
+      ),
 
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
-
+double abs(double x) {
+  return x < 0 ? -x : x;
+}
+Future<void> _popAd(BuildContext context, String title, String text) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(text),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+void _checkPoints(BuildContext context) async {
+  var location = new Location();
+  var pos = await location.getLocation();
+  if(instruction) {
+    _popAd(context, "Dziękujemy za pobranie aplikacji.", "Idź w świat i poznawaj kampus, przy okazji zbieraj punkty (klikając przy chodzeniu) i wymieniej je na piwo ;)");
+    instruction = false;
+  }
+//  if(abs(pos.latitude - 51.589825 ) < 0.00012 && abs(pos.longitude - 19.158243 ) < 0.00012) {
+//    _popAd(context, "Brawo", "Udało Ci się! Zdobyłeś 100 punktów!");
+//    userPoints += 100;
+//  }
+//  if( abs(pos.latitude - 51.590552 ) < 0.00052 && abs(pos.longitude - 19.158772 ) < 0.00052) {
+//    _popAd(context, "Niemożliwe", "Udało Ci się! Zdobyłeś 100 punktów!");
+//    userPoints += 100;
+//  }
+//  if( abs(pos.latitude - 51.590969) < 0.00052 && abs(pos.longitude - 19.159729 ) < 0.00052) {
+//    _popAd(context, "Zabka", "Udało Ci się! Zdobyłeś 100 punktów!");
+//    userPoints += 100;
+//  }
+//  if( abs(pos.latitude - 51.590459 ) < 0.00052 && abs(pos.longitude - 19.160708 ) < 0.00052) {
+//    _popAd(context, "łaka", "Udało Ci się! Zdobyłeś 100 punktów!");
+//    userPoints += 100;
+//  }
+//  if( abs(pos.latitude - 51.589092 ) < 0.00052 && abs(pos.longitude - 19.158801 ) < 0.00052) {
+//    _popAd(context, "Kotłownia", "Udało Ci się! Zdobyłeś 100 punktów!");
+//    userPoints += 100;
+//  }
+  if(abs(pos.latitude - 51.747179 ) < 0.00015 && abs(pos.longitude - 19.453392 ) < 0.00015) {
+    _popAd(context, "Brawo", "Udało Ci się znaleźć windę. Zdobyłeś 10 punktów! Czy wiedziałeś, że często się psują?");
+    userPoints += 10;
+  }
+  if( abs(pos.latitude - 51.747208 ) < 0.00015 && abs(pos.longitude - 19.453742 ) < 0.00015) {
+    _popAd(context, "Niemożliwe", "Udało Ci się zobaczyć Lodex => budenek trzech wydziałów! Zdobyłeś 20 punktów!");
+    userPoints += 10;
+  }
+  if( abs(pos.latitude - 51.747208) < 0.00015 && abs(pos.longitude - 19.453742 ) < 0.00015) {
+    _popAd(context, "Kącik sali.", "Stoisz w kącie! Zdobyłeś 10 punktów!");
+    userPoints += 10;
+  }
+}
 class ProfileMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -254,6 +335,7 @@ class FireMapState extends State<FireMap> {
   void _onMapCreated(GoogleMapController controller) {
     _addMarkers(controller);
     _animateToUser();
+    //_checkPoints();
     setState(() {
       mapController = controller;
     });
@@ -271,22 +353,96 @@ class FireMapState extends State<FireMap> {
   }
   void _addMarkers(GoogleMapController controller) {
     var marker = MarkerOptions(
-        position: LatLng(51.7474, 19.4537),
+        position: LatLng(51.747179, 19.453392),                                       //Polibuda
+//        position: LatLng(51.589825, 19.158243),                                     //Home
         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-        infoWindowText: InfoWindowText("Jej, udalo sie!","")
+        infoWindowText: InfoWindowText("O winda!","")
     );
     controller.addMarker(marker);
-  }
-  void _checkPoints() async {
-    var location = new Location();
-    var pos = await location.getLocation();
+    var marker2 = MarkerOptions(
+        position: LatLng(51.747201, 19.452759),                                       //Polibuda
+//        position: LatLng(51.590552, 19.158772),                                     //Home
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+        infoWindowText: InfoWindowText("Kącik.","")
+    );
+    controller.addMarker(marker2);
+    var marker3 = MarkerOptions(
+        position: LatLng(51.747208, 19.453742),                                       //Polibuda
+//        position: LatLng(51.590969, 19.159729),                                     //Home
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+        infoWindowText: InfoWindowText("Lodex","")
+    );
+    controller.addMarker(marker3);
 
-    if( abs(pos.latitude - 51.747 ) < 0.001 && abs(pos.longitude - 19.4537 ) < 0.001) {
-
-    }
   }
-  double abs(double x) {
-    return x < 0 ? -x : x;
-  }
-
+//  void _checkPoints() async {
+//    var location = new Location();
+//    var pos = await location.getLocation();
+//    if(instruction) {
+//      setState(() {
+//        _popAd(context, "Dziękujemy za pobranie aplikacji.", "Idź w świat i poznawaj kampus, przy okazji zbieraj punkty i wymieniej je na piwo ;)");
+//      });
+//      instruction = false;
+//    }
+//    if( abs(pos.latitude - 51.589825 ) < 0.00012 && abs(pos.longitude - 19.158243 ) < 0.00012) {
+//      setState(() {
+//        _popAd(context, "Brawo", "Udało Ci się! Zdobyłeś 100 punktów!");
+//      });
+//      userPoints += 100;
+//    }
+//    if( abs(pos.latitude - 51.590552 ) < 0.00052 && abs(pos.longitude - 19.158772 ) < 0.00052) {
+//      setState(() {
+//        _popAd(context, "Niemożliwe", "Udało Ci się! Zdobyłeś 100 punktów!");
+//      });
+//      userPoints += 100;
+//    }
+//    if( abs(pos.latitude - 51.590969) < 0.00052 && abs(pos.longitude - 19.159729 ) < 0.00052) {
+//      setState(() {
+//        _popAd(context, "Zabka", "Udało Ci się! Zdobyłeś 100 punktów!");
+//      });
+//      userPoints += 100;
+//    }
+//    if( abs(pos.latitude - 51.590459 ) < 0.00052 && abs(pos.longitude - 19.160708 ) < 0.00052) {
+//      setState(() {
+//        _popAd(context, "łaka", "Udało Ci się! Zdobyłeś 100 punktów!");
+//      });
+//      userPoints += 100;
+//    }
+//    if( abs(pos.latitude - 51.589092 ) < 0.00052 && abs(pos.longitude - 19.158801 ) < 0.00052) {
+//      setState(() {
+//        _popAd(context, "Kotłownia", "Udało Ci się! Zdobyłeś 100 punktów!");
+//      });
+//      userPoints += 100;
+//    }
+//
+//  }
+//  double abs(double x) {
+//    return x < 0 ? -x : x;
+//  }
+//  Future<void> _popAd(BuildContext context, String title, String text) async {
+//    return showDialog<void>(
+//      context: context,
+//      barrierDismissible: false, // user must tap button!
+//      builder: (BuildContext context) {
+//        return AlertDialog(
+//          title: Text(title),
+//          content: SingleChildScrollView(
+//            child: ListBody(
+//              children: <Widget>[
+//                Text(text),
+//              ],
+//            ),
+//          ),
+//          actions: <Widget>[
+//            FlatButton(
+//              child: Text('OK'),
+//              onPressed: () {
+//                Navigator.of(context).pop();
+//              },
+//            ),
+//          ],
+//        );
+//      },
+//    );
+//  }
 }
