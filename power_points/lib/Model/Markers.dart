089ee969df5ff32bc7dd5ecfd610186178c4c5f6
Szymon Gruda <szymon_gruda@o2.pl>
2019-12-18 12:48:22 +0100

    Refactoring
    Creating File Coipons.dart.

--- power_points/lib/Model/Markers.dart
+++ power_points/lib/Model/Markers.dart
@@ -1,24 +1,3 @@
-class Markers {
-  static Map<int, MarkerData> markers = {
-    0:  new MarkerData(
-          51.747179, 19.453392,
-          "O winda!", "",
-          "Brawo", "Udało Ci się znaleźć windę. Zdobyłeś 10 punktów! Czy wiedziałeś, że często się psują?",
-          10),
-    1:  new MarkerData(
-        51.747208, 19.453742,
-        "Lodex", "",
-        "Niemożliwe", "Udało Ci się zobaczyć Lodex => budenek trzech wydziałów! Zdobyłeś 20 punktów!",
-        10),
-    2:  new MarkerData(
-        51.747208, 19.453742,
-        "Kącik.", "",
-        "Kącik sali.", "Stoisz w kącie! Zdobyłeś 10 punktów!",
-        10)
-
-  };
-
-}
 class MarkerData {
   final double markerLatitude;
   final double markerLongitude;
@@ -32,4 +11,25 @@ class MarkerData {
   MarkerData(this.markerLatitude, this.markerLongitude, this.markerTitle,
       this.markerDescription, this.markerTitleAfterCheck,
       this.markerDescriptionAfterCheck, this.points);
+}
+class Markers {
+  static Map<int, MarkerData> markers = {
+    0:  new MarkerData(
+        51.747179, 19.453392,
+        "O winda!", "",
+        "Brawo", "Udało Ci się znaleźć windę. Zdobyłeś 10 punktów! Czy wiedziałeś, że często się psują?",
+        10),
+    1:  new MarkerData(
+        51.747208, 19.453742,
+        "Lodex", "",
+        "Niemożliwe", "Udało Ci się zobaczyć Lodex => budenek trzech wydziałów! Zdobyłeś 20 punktów!",
+        10),
+    2:  new MarkerData(
+        51.747208, 19.453742,
+        "Kącik.", "",
+        "Kącik sali.", "Stoisz w kącie! Zdobyłeś 10 punktów!",
+        10)
+
+  };
+
 }
