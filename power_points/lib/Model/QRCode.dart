e8a6e581f4d18bf61714ca7b6616a01053b172c7
Szymon Gruda <szymon_gruda@o2.pl>
2019-12-27 18:17:44 +0100

    Refactoring

--- power_points/lib/Model/QRCode.dart
+++ power_points/lib/Model/QRCode.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:flutter/services.dart';
-import 'dart:async';
-import 'dart:typed_data';
 import 'dart:ui';
-import 'dart:io';
 import 'package:flutter/rendering.dart';
-import 'package:path_provider/path_provider.dart';
 
 class GenerateScreen extends StatefulWidget {
   @override
@@ -21,8 +16,6 @@ class GenerateScreenState extends State<GenerateScreen> {
 
   GlobalKey globalKey = new GlobalKey();
   String _dataString = "Hello from this QR";
-  String _inputErrorText;
-  final TextEditingController _textController =  TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,33 +26,12 @@ class GenerateScreenState extends State<GenerateScreen> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.share),
-            onPressed: _captureAndSharePng,
           )
         ],
       ),
       body: _contentWidget(),
     );
   }
-
-  Future<void> _captureAndSharePng() async {
-    try {
-      RenderRepaintBoundary boundary = globalKey.currentContext.findRenderObject();
-      var image = await boundary.toImage();
-      ByteData byteData = await image.toByteData(format: ImageByteFormat.png);
-      Uint8List pngBytes = byteData.buffer.asUint8List();
-
-      final tempDir = await getTemporaryDirectory();
-      final file = await new File('${tempDir.path}/image.png').create();
-      await file.writeAsBytes(pngBytes);
-
-      final channel = const MethodChannel('channel:me.alfian.share/share');
-      channel.invokeMethod('shareFile', 'image.png');
-
-    } catch(e) {
-      print(e.toString());
-    }
-  }
-
   _contentWidget() {
     final bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
     return  Container(
